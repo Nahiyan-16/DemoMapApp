cbf8e647f79dc841025b28f122543b4606cedcb5
Nahiyan-16 <71025942+Nahiyan-16@users.noreply.github.com>
2023-01-19 19:38:31 -0500

    Added Demo Map

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView from 'react-native-maps'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <MapView style={styles.map} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-});
+  map: {
+    width: '100%',
+    height: '100%',
+  },
+})
