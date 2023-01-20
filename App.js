f09111a600c6d7aa258949636e33d6bcc5d3e749
Nahiyan-16 <71025942+Nahiyan-16@users.noreply.github.com>
2023-01-19 20:08:45 -0500

    Added permissions

--- App.js
+++ App.js
@@ -1,11 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import MapView from 'react-native-maps'
+import React from 'react';
+import * as Location from "expo-location"
 
 export default function App() {
+  const[locPermission, setLocPermission] = React.useState(false)
+
+  async function getPermission(){
+    const foregroundPermission = await Location.requestForegroundPermissionsAsync()
+    return foregroundPermission.granted
+  }
+
+React.useEffect(
+  ()=>{
+    async function start(){
+      const Permission = await getPermission()
+      setLocPermission(Permission)
+    }
+    start()
+  },
+  []
+)
   return (
     <View style={styles.container}>
-      <MapView style={styles.map} mapType='standard' showsUserLocation='true'/>
+      <MapView 
+        style={styles.map} 
+        mapType='standard' 
+        showsUserLocation={locPermission}
+        />
       <StatusBar style="auto" />
     </View>
   );
